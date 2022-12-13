b2264835eea7363f4637b1b349b6c5ea33531f7a
Jamalov <Jamalovn07@gmail.com>
2022-12-13 11:53:47 +0500

    bugs fixed and seat type module is checked

--- src/seat_type/seat_type.module.ts
+++ src/seat_type/seat_type.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SeatTypeService } from './seat_type.service';
 import { SeatTypeController } from './seat_type.controller';
+import { Seat_Type } from './seat_type.model';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Seat_Type])],
   controllers: [SeatTypeController],
-  providers: [SeatTypeService]
+  providers: [SeatTypeService],
 })
 export class SeatTypeModule {}
