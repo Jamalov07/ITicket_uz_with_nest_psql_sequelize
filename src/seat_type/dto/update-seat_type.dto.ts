12c9d90d29323d916539c5d1e56de26f0a864c42
Jamalov <Jamalovn07@gmail.com>
2022-12-13 11:48:51 +0500

    added seat type module

--- src/seat_type/dto/update-seat_type.dto.ts
+++ src/seat_type/dto/update-seat_type.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSeatTypeDto } from './create-seat_type.dto';
+// import { PartialType } from '@nestjs/mapped-types';
+// import { CreateSeatTypeDto } from './create-seat_type.dto';
 
-export class UpdateSeatTypeDto extends PartialType(CreateSeatTypeDto) {}
+// export class UpdateSeatTypeDto extends PartialType(CreateSeatTypeDto) {}
+
+import { IsOptional, IsString } from 'class-validator';
+
+export class UpdateSeatTypeDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+}
--- src/seat_type/seat_type.service.ts
+++ src/seat_type/seat_type.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateSeatTypeDto } from './dto/create-seat_type.dto';
 import { UpdateSeatTypeDto } from './dto/update-seat_type.dto';
+import { Seat_Type } from './seat_type.model';
 
 @Injectable()
 export class SeatTypeService {
-  create(createSeatTypeDto: CreateSeatTypeDto) {
-    return 'This action adds a new seatType';
+  constructor(
+    @InjectModel(Seat_Type)
+    private seat_typeRepo: typeof Seat_Type,
+  ) {}
+
+  async create(createSeat_typeDto: CreateSeatTypeDto) {
+    const candidate = await this.seat_typeRepo.findOne({
+      where: { ...createSeat_typeDto },
+    });
+    if (candidate) {
+      throw new BadRequestException('this datas already axists in database');
+    }
+    const newSeat_type = await this.seat_typeRepo.create(createSeat_typeDto);
+    return newSeat_type;
   }
 
-  findAll() {
-    return `This action returns all seatType`;
+  async findAll() {
+    const seat_types = await this.seat_typeRepo.findAll();
+    if (!seat_types) {
+      throw new BadRequestException('Seat_types not found');
+    }
+    return seat_types;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} seatType`;
+  async findOne(id: number) {
+    const seat_type = await this.seat_typeRepo.findOne({
+      where: { id: id },
+    });
+    if (!seat_type) {
+      throw new BadRequestException('Seat_type not found');
+    }
+    return seat_type;
   }
 
-  update(id: number, updateSeatTypeDto: UpdateSeatTypeDto) {
-    return `This action updates a #${id} seatType`;
+  async update(id: number, updateSeat_typeDto: UpdateSeatTypeDto) {
+    const seat_type = await this.seat_typeRepo.findOne({
+      where: { id: id },
+    });
+    if (!seat_type) {
+      throw new BadRequestException('Seat_type not found');
+    }
+
+    const candidate = await this.seat_typeRepo.findOne({
+      where: { ...updateSeat_typeDto },
+    });
+    if (candidate && candidate.id != id) {
+      throw new BadRequestException('This data already exists');
+    }
+
+    const updatedSeat_type = await (
+      await this.seat_typeRepo.update(updateSeat_typeDto, {
+        where: { id: id },
+        returning: true,
+      })
+    )[1][0];
+    return updatedSeat_type;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} seatType`;
+  async remove(id: number) {
+    const Seat_type = await this.seat_typeRepo.findOne({
+      where: { id: id },
+    });
+    if (!Seat_type) {
+      throw new BadRequestException('Seat_type not found');
+    }
+    await this.seat_typeRepo.destroy({ where: { id: id } });
+
+    return { message: 'Seat_type deleted', Seat_type };
   }
 }
