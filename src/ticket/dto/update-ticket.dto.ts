270d0e00d9a983f1d93f6bc586f28b6d9d5bb301
Jamalov <Jamalovn07@gmail.com>
2022-12-13 12:00:22 +0500

    added ticket module

--- src/ticket/dto/update-ticket.dto.ts
+++ src/ticket/dto/update-ticket.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTicketDto } from './create-ticket.dto';
+// import { PartialType } from '@nestjs/mapped-types';
+// import { CreateTicketDto } from './create-ticket.dto';
 
-export class UpdateTicketDto extends PartialType(CreateTicketDto) {}
+// export class UpdateTicketDto extends PartialType(CreateTicketDto) {}
+
+import { IsOptional, IsNumber } from 'class-validator';
+
+export class UpdateTicketDto {
+  @IsOptional()
+  @IsNumber()
+  event_id?: number;
+  @IsOptional()
+  @IsNumber()
+  seat_id?: number;
+  @IsOptional()
+  @IsNumber()
+  price?: number;
+  @IsOptional()
+  @IsNumber()
+  service_fee?: number;
+  @IsOptional()
+  @IsNumber()
+  status_id?: number;
+  @IsOptional()
+  @IsNumber()
+  ticket_type?: number;
+}
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
+import { Ticket } from './ticket.model';
 
 @Injectable()
 export class TicketService {
-  create(createTicketDto: CreateTicketDto) {
-    return 'This action adds a new ticket';
+  constructor(
+    @InjectModel(Ticket)
+    private ticketRepo: typeof Ticket,
+  ) {}
+
+  async create(createticketDto: CreateTicketDto) {
+    const candidate = await this.ticketRepo.findOne({
+      where: { ...createticketDto },
+    });
+    if (candidate) {
+      throw new BadRequestException('this datas already axists in database');
+    }
+    const newticket = await this.ticketRepo.create(createticketDto);
+    return newticket;
   }
 
-  findAll() {
-    return `This action returns all ticket`;
+  async findAll() {
+    const tickets = await this.ticketRepo.findAll();
+    if (!tickets) {
+      throw new BadRequestException('tickets not found');
+    }
+    return tickets;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ticket`;
+  async findOne(id: number) {
+    const ticket = await this.ticketRepo.findOne({
+      where: { id: id },
+    });
+    if (!ticket) {
+      throw new BadRequestException('ticket not found');
+    }
+    return ticket;
   }
 
-  update(id: number, updateTicketDto: UpdateTicketDto) {
-    return `This action updates a #${id} ticket`;
+  async update(id: number, updateticketDto: UpdateTicketDto) {
+    const ticket = await this.ticketRepo.findOne({
+      where: { id: id },
+    });
+    if (!ticket) {
+      throw new BadRequestException('ticket not found');
+    }
+
+    const candidate = await this.ticketRepo.findOne({
+      where: { ...updateticketDto },
+    });
+    if (candidate && candidate.id != id) {
+      throw new BadRequestException('This data already exists');
+    }
+
+    const updatedticket = await (
+      await this.ticketRepo.update(updateticketDto, {
+        where: { id: id },
+        returning: true,
+      })
+    )[1][0];
+    return updatedticket;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} ticket`;
+  async remove(id: number) {
+    const ticket = await this.ticketRepo.findOne({
+      where: { id: id },
+    });
+    if (!ticket) {
+      throw new BadRequestException('ticket not found');
+    }
+    await this.ticketRepo.destroy({ where: { id: id } });
+
+    return { message: 'ticket deleted', ticket };
   }
 }
